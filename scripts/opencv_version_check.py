b4861c9b0df2d2838cb5d924641c1abaa8557855
Randy Mackay <rmackay9@yahoo.com>
2014-05-19 16:01:56 +0900

    opencv_version_check: simply displays opencv version

---
+++ scripts/opencv_version_check.py
@@ -0,0 +1,14 @@
+#
+# opencv_version_check - displays the opencv version on the machine
+#
+#   Start from command line using 'python opencv_version_check.py'.
+
+import cv2
+import cv2.cv
+
+def main():
+    # opencv_version_check - prints opencv version 
+    print "OpenCV %s" % cv2.__version__
+
+if __name__ == "__main__":
+    main()
