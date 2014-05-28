ffcf92170b0532883ddaa2a605cc37bb2097c17d
Randy Mackay <rmackay9@yahoo.com>
2014-05-28 21:00:32 +0900

    setup: delete unnecessary opencv build scripts

--- setup/opencv_download.sh
+++
@@ -1,9 +0,0 @@
-version="$(wget -q -O - http://sourceforge.net/projects/opencvlibrary/files/opencv-unix | egrep -m1 -o '\"[0-9](\.[0-9])+' | cut -c2-)"
-echo "Installing OpenCV" $version
-mkdir OpenCV
-cd OpenCV
-echo "Downloading OpenCV" $version
-wget -O OpenCV-$version.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-"$version".zip/download
-echo "Installing OpenCV" $version
-unzip OpenCV-$version.zip
-cd opencv-$version
