397e43780f1dafc59fce5659369f5b66917fe9e7
Randy Mackay <rmackay9@yahoo.com>
2014-06-04 17:05:41 +0900

    start_mavproxy.sh and rc.local startup scripts

---
+++ startup/start_mavproxy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# start_mavproxy.sh
+#
+# This script starts up mavproxy as odroid and should be called from /etc/rc.local
+
+# give 10 seconds before starting
+sleep 10
+
+date
+export HOME=/home/odroid
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/local/bin
+PATH=$PATH:$HOME/GitHub/ardupilot-balloon-finder
+PATH=$PATH:$HOME/GitHub/ardupilot-balloon-finder/scripts
+export PATH
+echo "PATH:" $PATH
+
+PYTHONPATH=$PYTHONPATH:$HOME/GitHub/ardupilot-balloon-finder/scripts
+export PYTHONPATH
+echo "PYTHONPATH:" $PYTHONPATH
+
+cd $HOME
+echo $HOME
+
+ls -l /dev/ttyUSB*
+ls -l /dev/serial/by-id
+ls -l /dev/video*
+
+screen -d -m -s /bin/bash mavproxy.py --master=/dev/ttyUSB0 --baudrate 921600 --aircraft MyCopter
+
+echo "start_mavproxy.sh done"
